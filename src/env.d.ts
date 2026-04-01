2368227a1bc7a2245dd8c77be64c231cadc4464d
Craig Carroll <cc46808@gmail.com>
2026-04-01 16:44:11 -0400

    Set up per-site Sentry projects for app, help, and www

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+	readonly PUBLIC_SENTRY_ENABLED?: string;
+	readonly PUBLIC_SENTRY_DSN?: string;
+	readonly PUBLIC_SENTRY_ENVIRONMENT?: string;
+	readonly PUBLIC_SENTRY_TRACES_SAMPLE_RATE?: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
